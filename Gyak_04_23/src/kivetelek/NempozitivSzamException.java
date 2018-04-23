8540c3f775053a5965dc88fb8c7cf1737dc76c20
jbalazs97 <36412827+jbalazs97@users.noreply.github.com>
2018-04-23 17:06:13 +0200

    Gyakorlat_04_23

---
+++ Gyak_04_23/src/kivetelek/NempozitivSzamException.java
@@ -0,0 +1,10 @@
+package kivetelek;
+
+@SuppressWarnings("serial")
+public class NempozitivSzamException extends Exception {
+		public NempozitivSzamException(String uzenet) {
+			super(uzenet);
+			
+		}
+	
+}
