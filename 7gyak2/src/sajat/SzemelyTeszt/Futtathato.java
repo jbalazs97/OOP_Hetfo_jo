2899ad9782b4359d7f74f3865d485b92567a6560
jbalazs97 <36412827+jbalazs97@users.noreply.github.com>
2018-05-07 12:50:51 +0200

    7_gyak_2_feladat

---
+++ 7gyak2/src/sajat/SzemelyTeszt/Futtathato.java
@@ -0,0 +1,14 @@
+package sajat.SzemelyTeszt;
+
+import sajat.Gyermek;
+
+public class Futtathato {
+
+	public static void main(String[] args) {
+		Gyermek  gyerek1 = new Gyermek ("XY",12,"SCHOOOOL");
+	
+		System.out.println(gyerek1.getClass());
+		System.out.println(gyerek1);
+
+	}
+}
